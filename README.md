0f09e67d2bc6cb3097754e8d833d134014db4638
avan <sakaja_218@yahoo.com.tw>
2024-12-01 17:29:34 +0800

    1. Change model download from the wget command to the requests module functionality.
    2. Add inbrowser=True parameter to Gradio.
    
    3. Upgrade the project's Gradio version to 5.8.0
    
    4. Replace facexlib with the improved version implemented by sczhou.1. Add weights_only=True parameter to torch.load to avoid FutureWarning.
    
    5. Modify GFPGAN to a custom version to avoid encountering the error: ModuleNotFoundError: No module named 'torchvision.transforms.functional_tensor'.
    
    6. Change **basicsr** to the GitHub **BasicSR** version to avoid encountering the error: `ModuleNotFoundError: No module named 'torchvision.transforms.functional_tensor'`.
    
    7. Face Restoration and RealESR can now be freely combined in various ways, or one can be set to "None" to use only the other model.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ emoji: 📈
 colorFrom: blue
 colorTo: gray
 sdk: gradio
-sdk_version: 3.1.7
+sdk_version: 5.8.0
 app_file: app.py
 pinned: false
 license: apache-2.0
--- app.py
+++ app.py
@@ -1,55 +1,84 @@
 import os
-
+import gc
 import cv2
+import requests
+import numpy as np
 import gradio as gr
 import torch
-from basicsr.archs.srvgg_arch import SRVGGNetCompact
+import traceback
+from tqdm import tqdm
+from realesrgan.archs.srvgg_arch import SRVGGNetCompact
 from gfpgan.utils import GFPGANer
 from realesrgan.utils import RealESRGANer
+from basicsr.archs.rrdbnet_arch import RRDBNet
 
-os.system("pip freeze")
-# download weights
-if not os.path.exists('realesr-general-x4v3.pth'):
-    os.system("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth -P .")
-if not os.path.exists('GFPGANv1.2.pth'):
-    os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.2.pth -P .")
-if not os.path.exists('GFPGANv1.3.pth'):
-    os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth -P .")
-if not os.path.exists('GFPGANv1.4.pth'):
-    os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth -P .")
-if not os.path.exists('RestoreFormer.pth'):
-    os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth -P .")
-if not os.path.exists('CodeFormer.pth'):
-    os.system("wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/CodeFormer.pth -P .")
+# Define URLs and their corresponding local storage paths
+face_model = {
+    "GFPGANv1.2.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.2.pth",
+    "GFPGANv1.3.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth",
+    "GFPGANv1.4.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth",
+    "RestoreFormer.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth",
+    "CodeFormer.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/CodeFormer.pth",
+}
+realesr_model = {
+    "realesr-general-x4v3.pth": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth",
+    "realesr-animevideov3.pth": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth",
+    "RealESRGAN_x4plus_anime_6B.pth": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth",
+    "RealESRGAN_x2plus.pth": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth",
+    "RealESRNet_x4plus.pth": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth",
+    "RealESRGAN_x4plus.pth": "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth",
+    "4x-AnimeSharp.pth": "https://huggingface.co/utnah/esrgan/resolve/main/4x-AnimeSharp.pth?download=true",
+}
+files_to_download = [
+    ( "a1.jpg",
+      "https://thumbs.dreamstime.com/b/tower-bridge-traditional-red-bus-black-white-colors-view-to-tower-bridge-london-black-white-colors-108478942.jpg" ),
+    ( "a2.jpg",
+      "https://media.istockphoto.com/id/523514029/photo/london-skyline-b-w.jpg?s=612x612&w=0&k=20&c=kJS1BAtfqYeUDaORupj0sBPc1hpzJhBUUqEFfRnHzZ0=" ),
+    ( "a3.jpg",
+      "https://i.guim.co.uk/img/media/06f614065ed82ca0e917b149a32493c791619854/0_0_3648_2789/master/3648.jpg?width=700&quality=85&auto=format&fit=max&s=05764b507c18a38590090d987c8b6202" ),
+    ( "a4.jpg",
+      "https://i.pinimg.com/736x/46/96/9e/46969eb94aec2437323464804d27706d--victorian-london-victorian-era.jpg" ),
+]
 
-torch.hub.download_url_to_file(
-    'https://thumbs.dreamstime.com/b/tower-bridge-traditional-red-bus-black-white-colors-view-to-tower-bridge-london-black-white-colors-108478942.jpg',
-    'a1.jpg')
-torch.hub.download_url_to_file(
-    'https://media.istockphoto.com/id/523514029/photo/london-skyline-b-w.jpg?s=612x612&w=0&k=20&c=kJS1BAtfqYeUDaORupj0sBPc1hpzJhBUUqEFfRnHzZ0=',
-    'a2.jpg')
-torch.hub.download_url_to_file(
-    'https://i.guim.co.uk/img/media/06f614065ed82ca0e917b149a32493c791619854/0_0_3648_2789/master/3648.jpg?width=700&quality=85&auto=format&fit=max&s=05764b507c18a38590090d987c8b6202',
-    'a3.jpg')
-torch.hub.download_url_to_file(
-    'https://i.pinimg.com/736x/46/96/9e/46969eb94aec2437323464804d27706d--victorian-london-victorian-era.jpg',
-    'a4.jpg')
-
-# background enhancer with RealESRGAN
-model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-model_path = 'realesr-general-x4v3.pth'
-half = True if torch.cuda.is_available() else False
-upsampler = RealESRGANer(scale=4, model_path=model_path, model=model, tile=0, tile_pad=10, pre_pad=0, half=half)
-
+# Ensure the target directory exists
+os.makedirs("weights", exist_ok=True)
 os.makedirs('output', exist_ok=True)
 
+def download_from_url(output_path, url):
+    try:
+        # Check if the file already exists
+        if os.path.exists(output_path):
+            print(f"File already exists, skipping download: {output_path}")
+            return
 
-# def inference(img, version, scale, weight):
-def inference(img, version, scale):
-    # weight /= 100
+        print(f"Downloading: {url}")
+        with requests.get(url, stream=True) as response, open(output_path, "wb") as f:
+            total_size = int(response.headers.get('content-length', 0))
+            with tqdm(total=total_size, unit='B', unit_scale=True) as pbar:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+        print(f"Download successful: {output_path}")
+    except requests.RequestException as e:
+        print(f"Download failed: {url}, Error: {e}")
+
+
+# Iterate through each file
+for output_path, url in files_to_download:
+    # Check if the file already exists
+    if os.path.exists(output_path):
+        print(f"File already exists, skipping download: {output_path}")
+        continue
+
+    # Start downloading
+    download_from_url(output_path, url)
+
+
+def inference(img, version, realesr, scale: float):
     print(img, version, scale)
     try:
-        extension = os.path.splitext(os.path.basename(str(img)))[1]
+        img_name = os.path.basename(str(img))
+        basename, extension = os.path.splitext(img_name)
         img = cv2.imread(img, cv2.IMREAD_UNCHANGED)
         if len(img.shape) == 3 and img.shape[2] == 4:
             img_mode = 'RGBA'
@@ -62,50 +91,115 @@ def inference(img, version, scale):
         h, w = img.shape[0:2]
         if h < 300:
             img = cv2.resize(img, (w * 2, h * 2), interpolation=cv2.INTER_LANCZOS4)
+            
+        if version:
+            download_from_url(os.path.join("weights", version), face_model[version])
+        if realesr:
+            download_from_url(os.path.join("weights", realesr), realesr_model[realesr])
 
-        if version == 'v1.2':
-            face_enhancer = GFPGANer(
-            model_path='GFPGANv1.2.pth', upscale=2, arch='clean', channel_multiplier=2, bg_upsampler=upsampler)
-        elif version == 'v1.3':
-            face_enhancer = GFPGANer(
-            model_path='GFPGANv1.3.pth', upscale=2, arch='clean', channel_multiplier=2, bg_upsampler=upsampler)
-        elif version == 'v1.4':
-            face_enhancer = GFPGANer(
-            model_path='GFPGANv1.4.pth', upscale=2, arch='clean', channel_multiplier=2, bg_upsampler=upsampler)
-        elif version == 'RestoreFormer':
-            face_enhancer = GFPGANer(
-            model_path='RestoreFormer.pth', upscale=2, arch='RestoreFormer', channel_multiplier=2, bg_upsampler=upsampler)
-        elif version == 'CodeFormer':
-             face_enhancer = GFPGANer(
-             model_path='CodeFormer.pth', upscale=2, arch='CodeFormer', channel_multiplier=2, bg_upsampler=upsampler)
-        elif version == 'RealESR-General-x4v3':
-             face_enhancer = GFPGANer(
-             model_path='realesr-general-x4v3.pth', upscale=2, arch='realesr-general', channel_multiplier=2, bg_upsampler=upsampler)
+        # background enhancer with RealESRGAN
+        if realesr == 'RealESRGAN_x4plus.pth':  # x4 RRDBNet model
+            netscale = 4
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=netscale)
+        elif realesr == 'RealESRNet_x4plus.pth':  # x4 RRDBNet model
+            netscale = 4
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=netscale)
+        elif realesr == 'RealESRGAN_x4plus_anime_6B.pth':  # x4 RRDBNet model with 6 blocks
+            netscale = 4
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=netscale)
+        elif realesr == 'RealESRGAN_x2plus.pth':  # x2 RRDBNet model
+            netscale = 2
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=netscale)
+        elif realesr == 'realesr-animevideov3.pth':  # x4 VGG-style model (XS size)
+            netscale = 4
+            model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=netscale, act_type='prelu')
+        elif realesr == 'realesr-general-x4v3.pth':  # x4 VGG-style model (S size)
+            netscale = 4
+            model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=netscale, act_type='prelu')
+        # elif realesr == '4x-AnimeSharp.pth':  # 4x-AnimeSharp
+        #     netscale = 4
+        #     model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=netscale)
+        
+        half = True if torch.cuda.is_available() else False
+        upsampler = RealESRGANer(scale=netscale, model_path=os.path.join("weights", realesr), model=model, tile=0, tile_pad=10, pre_pad=0, half=half)
 
+        face_enhancer = None
+        if version == 'GFPGANv1.2.pth':
+            face_enhancer = GFPGANer(
+            model_path='weights/GFPGANv1.2.pth', upscale=scale, arch='clean', channel_multiplier=2, bg_upsampler=upsampler)
+        elif version == 'GFPGANv1.3.pth':
+            face_enhancer = GFPGANer(
+            model_path='weights/GFPGANv1.3.pth', upscale=scale, arch='clean', channel_multiplier=2, bg_upsampler=upsampler)
+        elif version == 'GFPGANv1.4.pth':
+            face_enhancer = GFPGANer(
+            model_path='weights/GFPGANv1.4.pth', upscale=scale, arch='clean', channel_multiplier=2, bg_upsampler=upsampler)
+        elif version == 'RestoreFormer.pth':
+            face_enhancer = GFPGANer(
+            model_path='weights/RestoreFormer.pth', upscale=scale, arch='RestoreFormer', channel_multiplier=2, bg_upsampler=upsampler)
+        elif version == 'CodeFormer.pth':
+             face_enhancer = GFPGANer(
+             model_path='weights/CodeFormer.pth', upscale=scale, arch='CodeFormer', channel_multiplier=2, bg_upsampler=upsampler)
+             
+        files = []
+        outputs = []
         try:
-            # _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True, weight=weight)
-            _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
+            if face_enhancer:
+                cropped_faces, restored_aligned, restored_img = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
+                # save faces
+                if cropped_faces and restored_aligned:
+                    for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_aligned)):
+                        # save cropped face
+                        save_crop_path = f"output/{basename}{idx:02d}_cropped_faces.png"
+                        cv2.imwrite(save_crop_path, cropped_face)
+                        # save restored face
+                        save_restore_path = f"output/{basename}{idx:02d}_restored_faces.png"
+                        cv2.imwrite(save_restore_path, restored_face)
+                        # save comparison image
+                        save_cmp_path = f"output/{basename}{idx:02d}_cmp.png"
+                        cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+                        cv2.imwrite(save_cmp_path, cmp_img)
+                
+                        files.append(save_crop_path)
+                        files.append(save_restore_path)
+                        files.append(save_cmp_path)
+                        outputs.append(cv2.cvtColor(cropped_face, cv2.COLOR_BGR2RGB))
+                        outputs.append(cv2.cvtColor(restored_face, cv2.COLOR_BGR2RGB))
+                        outputs.append(cv2.cvtColor(cmp_img, cv2.COLOR_BGR2RGB))
+            else:
+                restored_img, _ = upsampler.enhance(img, outscale=scale)
         except RuntimeError as error:
+            print(traceback.format_exc())
             print('Error', error)
+        finally:
+            if face_enhancer:
+                face_enhancer._cleanup()
+            else:
+                # Free GPU memory and clean up resources
+                torch.cuda.empty_cache()
+                gc.collect()
+
 
         try:
             if scale != 2:
                 interpolation = cv2.INTER_AREA if scale < 2 else cv2.INTER_LANCZOS4
                 h, w = img.shape[0:2]
-                output = cv2.resize(output, (int(w * scale / 2), int(h * scale / 2)), interpolation=interpolation)
+                restored_img = cv2.resize(restored_img, (int(w * scale / 2), int(h * scale / 2)), interpolation=interpolation)
         except Exception as error:
-            print('wrong scale input.', error)
-        if img_mode == 'RGBA':  # RGBA images should be saved in png format
-            extension = 'png'
-        else:
-            extension = 'jpg'
-        save_path = f'output/out.{extension}'
-        cv2.imwrite(save_path, output)
+            print(traceback.format_exc())
+            print("wrong scale input.", error)
 
-        output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
-        return output, save_path
+        if not extension:
+            extension = ".png" if img_mode == "RGBA" else ".jpg" # RGBA images should be saved in png format
+        save_path = f"output/{basename}{extension}"
+        cv2.imwrite(save_path, restored_img)
+
+        restored_img = cv2.cvtColor(restored_img, cv2.COLOR_BGR2RGB)
+        files.append(save_path)
+        outputs.append(restored_img)
+        return outputs, files
     except Exception as error:
-        print('global exception', error)
+        print(traceback.format_exc())
+        print("global exception", error)
         return None, None
 
 
@@ -122,21 +216,34 @@ article = r"""
 """
 demo = gr.Interface(
     inference, [
-        gr.inputs.Image(type="filepath", label="Input"),
-        # gr.inputs.Radio(['v1.2', 'v1.3', 'v1.4', 'RestoreFormer', 'CodeFormer'], type="value", default='v1.4', label='version'),
-        gr.inputs.Radio(['v1.2', 'v1.3', 'v1.4', 'RestoreFormer','CodeFormer','RealESR-General-x4v3'], type="value", default='v1.4', label='version'),
-        gr.inputs.Number(label="Rescaling factor", default=2),
-        # gr.Slider(0, 100, label='Weight, only for CodeFormer. 0 for better quality, 100 for better identity', default=50)
+        gr.Image(type="filepath", label="Input", format="png"),
+        gr.Dropdown(["GFPGANv1.2.pth",
+                     "GFPGANv1.3.pth",
+                     "GFPGANv1.4.pth",
+                     "RestoreFormer.pth",
+                     # "CodeFormer.pth",
+                     None], type="value", value='GFPGANv1.4.pth', label='Face Restoration version', info="Face Restoration and RealESR can be freely combined in different ways, or one can be set to \"None\" to use only the other model. Face Restoration is primarily used for face restoration in real-life images, while RealESR serves as a background restoration model."),
+        gr.Dropdown(["realesr-general-x4v3.pth",
+                     "realesr-animevideov3.pth",
+                     "RealESRGAN_x4plus_anime_6B.pth",
+                     "RealESRGAN_x2plus.pth",
+                     "RealESRNet_x4plus.pth",
+                     "RealESRGAN_x4plus.pth",
+                     # "4x-AnimeSharp.pth",
+                     None], type="value", value='realesr-general-x4v3.pth', label='RealESR version'),
+        gr.Number(label="Rescaling factor", value=2),
+        # gr.Slider(0, 100, label='Weight, only for CodeFormer. 0 for better quality, 100 for better identity', value=50)
     ], [
-        gr.outputs.Image(type="numpy", label="Output (The whole image)"),
-        gr.outputs.File(label="Download the output image")
+        gr.Gallery(type="numpy", label="Output (The whole image)", format="png"),
+        gr.File(label="Download the output image")
     ],
     title=title,
     description=description,
     article=article,
-    # examples=[['AI-generate.jpg', 'v1.4', 2, 50], ['lincoln.jpg', 'v1.4', 2, 50], ['Blake_Lively.jpg', 'v1.4', 2, 50],
-    #           ['10045.png', 'v1.4', 2, 50]]).launch()
-    examples=[['a1.jpg', 'v1.4', 2], ['a2.jpg', 'v1.4', 2], ['a3.jpg', 'v1.4', 2],['a4.jpg', 'v1.4', 2]])
+    examples=[['a1.jpg', 'GFPGANv1.4.pth', "realesr-general-x4v3.pth", 2], 
+              ['a2.jpg', 'GFPGANv1.4.pth', "realesr-general-x4v3.pth", 2], 
+              ['a3.jpg', 'GFPGANv1.4.pth', "realesr-general-x4v3.pth", 2],
+              ['a4.jpg', 'GFPGANv1.4.pth', "realesr-general-x4v3.pth", 2]])
     
-demo.queue(concurrency_count=4)
-demo.launch()
+demo.queue(default_concurrency_limit=4)
+demo.launch(inbrowser=True)
